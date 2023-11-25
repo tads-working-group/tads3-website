308de000f77903ccb7a1be0b81b2a0f2a0143f2f
pieartsy <17207322+pieartsy@users.noreply.github.com>
2023-11-24 20:31:30 -0500

    Update pandoc.bat

--- pandoc.bat
+++ pandoc.bat
@@ -6,9 +6,9 @@ FOR /f "delims=" %%a IN (
  'dir /b /s /a-d "*.md" "*.html" "*.htm" ^| findstr /v /i /c:"\\tourguide" /c:"\\libref\\"'
  ) DO (
  IF /i "%%~xa"==".md" (
-  IF NOT EXIST "%%~dpna.html" ECHO pandoc "%%a" -t gfm-raw_html -o "%%~dpna.md"
+  IF NOT EXIST "%%~dpna.html" ECHO pandoc "%%a" -t html -o "%%~dpna.md"
  ) ELSE (
-  IF NOT EXIST "%%~dpna.md" pandoc "%%a" -t gfm-raw_html -o "%%~dpna.md"
+  IF NOT EXIST "%%~dpna.md" pandoc "%%a" -t html -o "%%~dpna.md"
  )
 )
 popd
